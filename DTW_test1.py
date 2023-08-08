4fe1dd0e4bca3e4cce5e5c5848143426c85d9ec4
Jinwoo23 <122088202+Jinwoo23@users.noreply.github.com>
2023-08-08 16:39:50 +0900

    Update DTW_test1.py

--- DTW_test1.py
+++ DTW_test1.py
@@ -3,70 +3,21 @@ import dtw
 import pandas as pd
 
 
-#file_path1 = '/home/cps/Qgroundcontrol/vulnerable_case_flight_log/param_update_vul_case1-1/param_update_vul_case1-1_estimator_local_position_0.csv'
-#file_path2 = '/home/cps/Qgroundcontrol/vulnerable_case_flight_log/param_update_vul_case1_normal/param_update_vul_case1_normal_estimator_local_position_0.csv'
 
-file1_1 = '/home/cps/Qgroundcontrol/evalutaion/attitude_log_1_desired.csv'
-file1_2 = '/home/cps/Qgroundcontrol/evalutaion/attitude_log_1_estimated.csv'
+file1_1 = '/home/cps/Qgroundcontrol/evaluation/attitude_log_1_desired.csv'
+file1_2 = '/home/cps/Qgroundcontrol/evaluation/attitude_log_1_estimated.csv'
 
-file2_1 = '/home/cps/Qgroundcontrol/evalutaion/attitude_log_2_desired.csv'
-file2_2 = '/home/cps/Qgroundcontrol/evalutaion/attitude_log_2_estimated.csv'
+file2_1 = '/home/cps/Qgroundcontrol/evaluation/attitude_log_2_desired.csv'
+file2_2 = '/home/cps/Qgroundcontrol/evaluation/attitude_log_2_estimated.csv'
 
-file3_1 = '/home/cps/Qgroundcontrol/evalutaion/attitude_log_3_desired.csv'
-file3_2 = '/home/cps/Qgroundcontrol/evalutaion/attitude_log_3_estimated.csv'
+file3_1 = '/home/cps/Qgroundcontrol/evaluation/attitude_log_3_desired.csv'
+file3_2 = '/home/cps/Qgroundcontrol/evaluation/attitude_log_3_estimated.csv'
 
 file4_1 = '/home/cps/Qgroundcontrol/evaluation/attitude_log_4_desired.csv'
-file4_2 = '/home/cps/Qgroundcontrol/evalutaion/attitude_log_4_estimated.csv'
+file4_2 = '/home/cps/Qgroundcontrol/evaluation/attitude_log_4_estimated.csv'
 
 
-#df1 = pd.read_csv(file_path1)
-#df2 = pd.read_csv(file_path2)
 
-"""
-df1_1 = pd.read_csv(file1_1)
-df1_2 = pd.read_csv(file1_2)
-
-df2_1 = pd.read_csv(file2_1)
-df2_2 = pd.read_csv(file2_2)
-
-df3_1 = pd.read_csv(file3_1)
-df3_2 = pd.read_csv(file3_2)
-
-
-#desired만 받아옴
-q1_1d = df1_1.iloc[1:-1, 1]
-q1_2d = df2_1.iloc[1:-1, 1]
-q1_3d = df3_1.iloc[1:-1, 1]
-
-q2_1d = df1_1.iloc[1:-1, 2]
-q2_2d = df2_1.iloc[1:-1, 2]
-q2_3d = df3_1.iloc[1:-1, 2]
-
-q3_1d = df1_1.iloc[1:-1, 3]
-q3_2d = df2_1.iloc[1:-1, 3]
-q3_3d = df3_1.iloc[1:-1, 3]
-
-q4_1d = df1_1.iloc[1:-1, 4]
-q4_2d = df2_1.iloc[1:-1, 4]
-q4_3d = df3_1.iloc[1:-1, 4]
-
-#estimated만 받아옴
-q1_1 = df1_2.iloc[1:-1, 1]
-q1_2 = df2_2.iloc[1:-1, 1]
-q1_3 = df3_2.iloc[1:-1, 1]
-
-q2_1 = df1_2.iloc[1:-1, 2]
-q2_2 = df2_2.iloc[1:-1, 2]
-q2_3 = df3_2.iloc[1:-1, 2]
-
-q3_1 = df1_2.iloc[1:-1, 3]
-q3_2 = df2_2.iloc[1:-1, 3]
-q3_3 = df3_2.iloc[1:-1, 3]
-
-q4_1 = df1_2.iloc[1:-1, 4]
-q4_2 = df2_2.iloc[1:-1, 4]
-q4_3 = df3_2.iloc[1:-1, 4]
-"""
 
 # Load the desired and estimated attitude logs for the first case
 desired1 = pd.read_csv(file1_1)
@@ -173,6 +124,7 @@ print("q1_4 DTW distance value to check validation: ", q1_dtw1_4)
 print("q2_4 DTW distance value to check validation: ", q2_dtw1_4)
 print("q3_4 DTW distance value to check validation: ", q3_dtw1_4)
 print("q4_4 DTW distance value to check validation: ", q4_dtw1_4)
+print("----------------------------")
 
 
 if (q1_dtw1_1 > 15) or (q2_dtw1_1 > 15) or (q3_dtw1_1 > 15) or (q4_dtw1_1 > 15):
